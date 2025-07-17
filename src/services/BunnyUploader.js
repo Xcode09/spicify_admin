10dc29f04cbff96f760281ff5ddec760a43f2fee
Muhammad Ali Rana <alisaleem877@gmail.com>
2025-07-17 11:00:35 +0500

    File Upload error
    Fixed

--- src/services/BunnyUploader.js
+++ src/services/BunnyUploader.js
@@ -2,28 +2,28 @@
 
 export default class BunnyUploader {
   static async upload(file, path) {
-    // const bunnyStorageZone = "spicify-app";
-    // const bunnyAccessKey = "1a96a931-81ed-4517-be6f3e2ed8de-7fde-4ba4";
-    // const bunnyHostname = "spicifyapp.b-cdn.net";
+    const bunnyStorageZone = "spicify-app";
+    const bunnyAccessKey = "1a96a931-81ed-4517-be6f3e2ed8de-7fde-4ba4";
+    const bunnyHostname = "spicifyapp.b-cdn.net";
 
-    // if (!file) throw new Error("No file provided");
+    if (!file) throw new Error("No file provided");
 
-    // const response = await fetch(
-    //   `https://${bunnyHostname}/${bunnyStorageZone}/${path}`,
-    //   {
-    //     method: "PUT",
-    //     headers: {
-    //       AccessKey: bunnyAccessKey,
-    //       "Content-Type": file.type,
-    //     },
-    //     body: file,
-    //   }
-    // );
+    const response = await fetch(
+      `https://${bunnyHostname}/${bunnyStorageZone}/${path}`,
+      {
+        method: "PUT",
+        headers: {
+          AccessKey: bunnyAccessKey,
+          "Content-Type": file.type,
+        },
+        body: file,
+      }
+    );
 
-    // if (!response.ok) {
-    //   throw new Error(`Bunny upload failed: ${response.statusText}`);
-    // }
+    if (!response.ok) {
+      throw new Error(`Bunny upload failed: ${response.statusText}`);
+    }
 
-    return 'https://picsum.photos/seed/picsum/200/300';  //`https://${bunnyStorageZone}.b-cdn.net/${path}`;
+    return `https://spicifyapp.b-cdn.net/${path}`;
   }
 }
